632a293549de7dee2867b63c52d5c91f6e17aa05
kadiri abiola akanji <akanjikadiriabiola@gmail.com>
2024-07-16 09:34:42 +0000

    created branche

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dbname = "cfge";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
